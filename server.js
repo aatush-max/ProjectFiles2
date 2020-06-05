41da67da6db62d330b09fb98cbb8c0c020088061
aatush-max <65264475+aatush-max@users.noreply.github.com>
2020-06-05 21:05:03 +1000

    deploying to heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -5,6 +6,3 @@ app.use(express.static(__dirname+'/dist/footballdata'));
 app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname+'/dist/footballdata/index.html'));});
 app.listen(process.env.PORT|| 8080);
-
-
-
